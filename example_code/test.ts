d5f3df529c43281d61d298ddcb2f2b002c67f600
tada-js <justfix010@gmail.com>
2022-05-22 03:12:15 +0900

    :memo: 추가 예제 코드
    예제 코드 파일 추가

---
+++ example_code/test.ts
@@ -0,0 +1,23 @@
+// let member: string[] = ["김멋사", "이멋사", "박멋사"];
+// member.push("라이캣");
+
+// console.log(member);
+
+// let member: any[] = ["김멋사", 10, true, null];
+// let member: (string | number | boolean | null)[] = ["김멋사", 10, true, null];
+
+// let str: Array<string> = ["김멋사"];
+
+// let str: Array<string | number> = ["김멋사", 10];
+// let str2: typeof str = ["김멋사", 20];
+
+// console.log(str);
+// console.log(str2);
+
+// let arr: Array<() => string> = [() => "라이캣", () => "김멋사"];
+// console.log(arr[0]());
+
+let arr2: Array<string> = new Array<string>();
+arr2.push("one");
+arr2.push("two");
+console.log(arr2);
